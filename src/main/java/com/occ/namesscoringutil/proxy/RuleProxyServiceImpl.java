cc6e88b0e6a5f803a17f7f5c88c766b4444bdbbc
Martins Umachi <martins.umachi@umnisoft.com>
2020-07-01 09:48:47 -0500

    Added some logs

--- src/main/java/com/occ/namesscoringutil/proxy/RuleProxyServiceImpl.java
+++ src/main/java/com/occ/namesscoringutil/proxy/RuleProxyServiceImpl.java
@@ -12,26 +12,28 @@ public class RuleProxyServiceImpl implements RuleProxyService {
 
     final static Logger LOGGER = LoggerFactory.getLogger(RuleProxyServiceImpl.class);
 
-
-
     public long registerAndProcessScoringRules(Score score) {
 
         RuleEngine ruleEngine = new RuleEngine();
         Long result = 0L;
-        if (score.getScoreType().equals(Score.ScoreType.CURRENT)) {
-            ruleEngine
-                    .registerRule(new CurrentScoringRule(score));
-            result = ruleEngine.rule(score);
-        }
-        if (score.getScoreType().equals(Score.ScoreType.FUTURE)) {
-            ruleEngine
-                    .registerRule(new FutureScoringRule(score));
-            result = ruleEngine.rule(score);
-        }
-        if (score.getScoreType().equals(Score.ScoreType.ANOTHERDEPARTMENT)) {
-            ruleEngine
-                    .registerRule(new AnotherDepartmentScoringRule(score));
-            result = ruleEngine.rule(score);
+        try {
+            if (score.getScoreType().equals(Score.ScoreType.CURRENT)) {
+                ruleEngine
+                        .registerRule(new CurrentScoringRule(score));
+                result = ruleEngine.rule(score);
+            }
+            if (score.getScoreType().equals(Score.ScoreType.FUTURE)) {
+                ruleEngine
+                        .registerRule(new FutureScoringRule(score));
+                result = ruleEngine.rule(score);
+            }
+            if (score.getScoreType().equals(Score.ScoreType.ANOTHERDEPARTMENT)) {
+                ruleEngine
+                        .registerRule(new AnotherDepartmentScoringRule(score));
+                result = ruleEngine.rule(score);
+            }
+        } catch (Exception e) {
+            LOGGER.info("Error Processing your Request " + e.getMessage());
         }
 
         LOGGER.info(" Result Is " + result);
--- src/main/java/com/occ/namesscoringutil/rule/RuleEngine.java
+++ src/main/java/com/occ/namesscoringutil/rule/RuleEngine.java
@@ -1,27 +1,38 @@
 package com.occ.namesscoringutil.rule;
 
+import com.occ.namesscoringutil.proxy.RuleProxyServiceImpl;
 import com.occ.namesscoringutil.score.Score;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RuleEngine {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(RuleEngine.class);
 
-        public RuleEngine() {
+
+    public RuleEngine() {
             rules = new ArrayList<>();
         }
 
         List<IRule<Score, Long>> rules;
+        Long result = 0L;
         public Long rule(Score score) {
-            Long result = 0L;
-            for (int i = 0; i < rules.size(); i++) {
-                if (rules.get(i).toString().toLowerCase().contains(score.getScoreType().toString().toLowerCase())) {
-                    result = rules.get(i).processScore(score);
-                } else {
+            try {
 
-                    new RuntimeException("No Matching rule found");
+                for (int i = 0; i < rules.size(); i++) {
+                    if (rules.get(i).toString().toLowerCase().contains(score.getScoreType().toString().toLowerCase())) {
+                        result = rules.get(i).processScore(score);
+                    } else {
+
+                        new RuntimeException("No Matching rule found");
+                    }
                 }
+            }
+            catch (Exception e) {
+                LOGGER.info("There was a Problem in the Rule Engine " + e.getMessage());
             }
                 return result;
         }
