9df797657aeaf6ebfe6296c0c38aff801abc8ecd
Martins Umachi <martins.umachi@umnisoft.com>
2022-01-08 22:51:55 -0600

    Modified to curly braces in test script

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo This Version is $(VERSION)
-echo This proxy is $(BD_PROXY)
-echo This token is $(BD_TOKEN)
-echo This trust store path is $(MAVEN_TRUST_STORE)
-echo This key store path is $(MAVEN_KEY_STORE)
-echo This settings xml path is $(MAVEN_SETTINGS_XML)
+echo This Version is ${VERSION}
+echo This proxy is ${BD_PROXY}
+echo This token is ${BD_TOKEN}
+echo This trust store path is ${MAVEN_TRUST_STORE}
+echo This key store path is ${MAVEN_KEY_STORE}
+echo This settings xml path is ${MAVEN_SETTINGS_XML}
